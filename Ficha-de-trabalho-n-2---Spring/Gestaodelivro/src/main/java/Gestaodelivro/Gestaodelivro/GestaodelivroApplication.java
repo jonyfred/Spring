fed937f1bad3fe84e368b455d6038af16d49d336
jonyfred <5267@eclisboa.net>
2024-09-06 11:08:01 +0100

    Add files via upload

---
+++ Ficha-de-trabalho-n-2---Spring/Gestaodelivro/src/main/java/Gestaodelivro/Gestaodelivro/GestaodelivroApplication.java
@@ -0,0 +1,13 @@
+package Gestaodelivro.Gestaodelivro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaodelivroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaodelivroApplication.class, args);
+	}
+
+}
---
+++ Ficha-de-trabalho-n-2---Spring/Gestaodelivro/src/test/java/Gestaodelivro/Gestaodelivro/GestaodelivroApplicationTests.java
@@ -0,0 +1,13 @@
+package Gestaodelivro.Gestaodelivro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaodelivroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
